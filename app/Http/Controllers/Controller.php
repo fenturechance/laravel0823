d543f383dea18f20a69363b1623cf7b538bf1962
fenturechance <fenturechance@gmail.com>
2017-08-23 14:40:37 +0800

    MVC試成功

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+/*keyword same function name in many*/
 
+/*specific class namesapce import this class*/
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
--- routes/web.php
+++ routes/web.php
@@ -15,21 +15,29 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    return 'hi aboutpage';
-});
+// Route::get('/about', function () {
+//     return 'hi aboutpage';
+// });
 
-Route::get('/contact', function () {
-    return 'hi i am contact';
-});
+// Route::get('/contact', function () {
+//     return 'hi i am contact';
+// });
 
-Route::get('/post/{id}/{name}', function ($id,$name) {
-    return "this is post number".$id." ".$name;
-});
+// Route::get('/post/{id}/{name}', function ($id,$name) {
+//     return "this is post number".$id." ".$name;
+// });
 
-Route::get('admin/posts/example',array('as'=>'admin.home' ,function () {
+// Route::get('admin/posts/example',array('as'=>'admin.home' ,function () {
     
-	$url = route('admin.home');
+// 	$url = route('admin.home');
 
-	return "this url is".$url;
-}));
+// 	return "this url is".$url;
+// }));
+
+/*第二個參數直接放controller和他的method*/
+/*網址輸入的值可以直接拿去controller用*/
+// Route::get('/post/{id}', 'PostController@index');
+
+Route::resource('posts','PostController');
+
+Route::get('/contact','PostController@contact');
