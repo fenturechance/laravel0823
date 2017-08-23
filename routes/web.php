616c29b8bda17caee5b14feb36164209047201dd
fenturechance <fenturechance@gmail.com>
2017-08-23 11:24:03 +0800

    測試把route取名字

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,22 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return 'hi aboutpage';
+});
+
+Route::get('/contact', function () {
+    return 'hi i am contact';
+});
+
+Route::get('/post/{id}/{name}', function ($id,$name) {
+    return "this is post number".$id." ".$name;
+});
+
+Route::get('admin/posts/example',array('as'=>'admin.home' ,function () {
+    
+	$url = route('admin.home');
+
+	return "this url is".$url;
+}));
