1bcd5e907e1e0abcfb0115f16b700861d9dd5bb5
fenturechance <fenturechance@gmail.com>
2017-08-24 00:13:54 +0800

    migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+        Schema::create('users', function (Blueprint $table) {   
+            $table->increments('id'); 
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('email')->unique(); 
+            $table->string('password',60);
+            $table->rememberToken(); 
+            $table->timestamps(); 
         });
     }
 
@@ -28,7 +28,7 @@ class CreateUsersTable extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down() 
     {
         Schema::dropIfExists('users');
     }
